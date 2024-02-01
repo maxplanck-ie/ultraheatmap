77d8ca17233edb9d909956da05dd5355aa6fefb3
WardDeb <ward@deboutte.be>
2024-02-01 10:41:37 +0100

    change paths in test data relative to the repo root

--- ultraheatmap/test/test_data/addFeatureToMatrix/addFeatureToMatrix.sh
+++ ultraheatmap/test/test_data/addFeatureToMatrix/addFeatureToMatrix.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-computeMatrix reference-point -S ../scores.bw -R ../regions.bed -o matrix.gz
-addFeatureToMatrix -m matrix.gz -o appended_matrix.gz -t features.bed -f score1 score2 --featureIdColumn name
+computeMatrix reference-point -S ultraheatmap/test/test_data/scores.bw -R ultraheatmap/test/test_data/regions.bed -o matrix.gz
+addFeatureToMatrix -m matrix.gz -o appended_matrix.gz -t ultraheatmap/test/test_data/addFeatureToMatrix/features.bed -f score1 score2 --featureIdColumn name
